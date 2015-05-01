a5521a16e4b5ee72d08813af6f53c2dc3db5a160
Attila Ancsin <attilaancsin@gmail.com>
2015-05-01 20:01:38 +0200

    Advancing of checkers logic

---
+++ CheckersGame/Logic/StartingStateBuilder.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckersGame.Logic
+{
+    public static class StartingStateBuilder
+    {
+        private static GameSpace StartingState;
+
+        public static void BuildStartingState(GameSpace state)
+        {
+            StartingState = state;
+            PutUpBlacks();
+            PutUpWhites();
+        }
+
+        private static void PutUpBlacks()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    PutUpBlackIfStartPosition(i, j);
+                }
+            }
+        }
+
+        private static void PutUpBlackIfStartPosition(int i, int j)
+        {
+            if (i % 2 == 0)
+            {
+                if (j % 2 != 0)
+                {
+                    Position p = new Position(i, j);
+                    Element e = new Element(0, 0, 0);
+                    StartingState.setElementAt(p, e);
+                }
+            }
+            else
+            {
+                if (j % 2 == 0)
+                {
+                    Position p = new Position(i, j);
+                    Element e = new Element(0, 0, 0);
+                    StartingState.setElementAt(p, e);
+                }
+            }
+        }
+
+        private static void PutUpWhites()
+        {
+            for (int i = 5; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    PutUpWhiteIfStartPosition(i, j);
+                }
+            }
+        }
+
+        private static void PutUpWhiteIfStartPosition(int i, int j)
+        {
+            if (i % 2 == 0)
+            {
+                if (j % 2 != 0)
+                {
+                    Position p = new Position(i, j);
+                    Element e = new Element(0, 0, 1);
+                    StartingState.setElementAt(p, e);
+                }
+            }
+            else
+            {
+                if (j % 2 == 0)
+                {
+                    Position p = new Position(i, j);
+                    Element e = new Element(0, 0, 1);
+                    StartingState.setElementAt(p, e);
+                }
+            }
+        }
+    }
+}
---
+++ CheckersGame/Logic/StepSupervisor.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckersGame.Logic
+{
+    public static class StepSupervisor
+    {
+        public static int BoardMinIndex = 0;
+        public static int BoardMaxIndex = 7;
+
+        public static bool IsValidStep(Step step)
+        {
+            if (IsValidMove(step) || IsValidCapture(step))
+                return true;
+            else
+                return false;
+        }
+
+        private static bool IsValidMove(Step step)
+        {
+            if (IsDiagonal(step) && IsAdjacent(step) && !IsOccupied(step))
+                return true;
+            else
+                return false;
+        }
+
+        private static bool IsValidCapture(Step step)
+        {
+            if (IsDiagonal(step) && IsAdjacentAdjacent(step) && !IsOccupied(step) && IsThereAnOpponentBetween(step))
+                return true;
+            else
+                return false;
+        }
+
+        private static bool IsDiagonal(Step step)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static bool IsAdjacent(Step step)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static bool IsOccupied(Step step)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static bool IsOpponent(Step step)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static bool IsAdjacentAdjacent(Step step)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static bool IsThereAnOpponentBetween(Step step)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

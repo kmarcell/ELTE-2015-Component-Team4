4fa2ea3159ebf2983a62bcb8266e9eb97da39c03
mvasaros-pc <mate.vasaros@gmail.com>
2015-05-10 17:46:28 +0200

    add unit test project for platform

---
+++ Platform/PlatformUnitTests/MaintainCurrentGameTest.cs
@@ -0,0 +1,44 @@
+﻿using GTInterfacesLibrary;
+using GTInterfacesLibrary.MessageTypes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Platform.Model;
+
+namespace PlatformUnitTests
+{
+    public partial class PlatformUnitTests
+    {
+
+        [TestMethod]
+        public void MaintainCurrentGameTest()
+        {
+            NetworkManager = new NetworkManager();
+            GameManager = new GameManager(NetworkManager);
+
+            Assert.IsTrue(GameManager.CurrentGame == null, "The current game is not null, adfer creating new instance of Gamemanager.");
+
+            const int firstGameId = 1;
+            const string firstGameName = "1Game";
+            const string firstGameDescription = "1GameDescription";
+            var testGame1 = new TestGame(firstGameId, firstGameName, firstGameDescription);
+            GameManager.SetCurrentGame(testGame1);
+            Assert.IsTrue(GameManager.CurrentGame != null, "Set current game failed.");
+            Assert.IsTrue(GameManager.CurrentGame.Id == firstGameId, "Set current game faild due to ID mismatch.");
+            Assert.IsTrue(GameManager.CurrentGame.Name == firstGameName, "Set current game failed due to Name mismatch.");
+            Assert.IsTrue(GameManager.CurrentGame.Description == firstGameDescription, "Set current game failed due ti Description mismatch.");
+
+
+            const int secondGameId = 2;
+            const string secondGameName = "2Game";
+            const string secondGameDescription = "2GameDescription";
+            var testGame2 = new TestGame(secondGameId, secondGameName, secondGameDescription);
+            GameManager.SetCurrentGame(testGame2);
+            Assert.IsTrue(GameManager.CurrentGame != null, "Set current game failed.");
+            Assert.IsTrue(GameManager.CurrentGame.Id == secondGameId, "Set current game faild due to ID mismatch.");
+            Assert.IsTrue(GameManager.CurrentGame.Name == secondGameName, "Set current game failed due to Name mismatch.");
+            Assert.IsTrue(GameManager.CurrentGame.Description == secondGameDescription, "Set current game failed due ti Description mismatch.");
+
+
+
+        }
+    }
+}
---
+++ Platform/PlatformUnitTests/PlatformUnitTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using GTInterfacesLibrary;
+using GTInterfacesLibrary.GameEvents;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Platform.Model.Interface;
+
+namespace PlatformUnitTests
+{
+    [TestClass]
+    public partial class PlatformUnitTests
+    {
+        private IGameManager GameManager;
+        private INetworkManager NetworkManager;
+
+        private class TestGame : GTGameInterface
+        {
+            private GTPlatformManagerInterface PlatformGameManager;
+
+            public TestGame(Int32 id, String name, String description)
+            {
+                Id = id;
+                Name = name;
+                Description = description;
+            }
+
+            public event EventHandler<GameStateChangedEventArgs> SendGameStateChangedEventArg;
+            public void SendGameState(GameStateChangedEventArgs currentGameStateChangedEventArgs)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void RegisterGameManager(GTPlatformManagerInterface platformGameManager)
+            {
+                PlatformGameManager = platformGameManager;
+            }
+
+            public void RegisterArtificialIntelligence(IGTArtificialIntelligenceInterface artificialIntelligence)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void RegisterGui(GTGuiInterface gui)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void RecieveGameState(object sender, GameStateChangedEventArgs gameStateChangedEventArgs)
+            {
+                throw new NotImplementedException();
+            }
+
+            public string Name { get; private set; }
+            public int Id { get; private set; }
+            public string Description { get; private set; }
+            public void LoadGame(byte[] gameState)
+            {
+                throw new NotImplementedException();
+            }
+
+            public byte[] SaveGame()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
